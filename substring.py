96523d1726905e705ebd3fa4a82d0fb80a225717
Karthik <karthik.dot@gmail.com>
2016-04-06 21:04:50 -0700

    substring program full

--- substring.py
+++ substring.py
@@ -4,3 +4,13 @@ def isSubString(stringInp, mainString):
     return False
 
 def main():
+    mainString = raw_input("Enter main string : ")
+    stringInp = raw_input("Enter substring: ")
+    if isSubString(stringInp, mainString):
+        print "it is a substring"
+    else:
+        print "Not a substring"
+    return
+
+if __name__ == "__main__":
+    main()
