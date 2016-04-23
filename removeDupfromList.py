8be1ca64e9dd28e34ba5d53159c45c5036eae527
Karthik <karthik.dot@gmail.com>
2016-04-23 14:55:08 -0700

    remove duplicates from sorted list, return len - leetcode challenge

---
+++ removeDupfromList.py
@@ -0,0 +1,25 @@
+# Given a sorted array, remove the duplicates in place such that each element
+#appear only once and return the new length.
+#
+# Do not allocate extra space for another array, you must do this in place with
+#constant memory.
+#
+# For example,
+# Given input array nums = [1,1,2],
+#
+# Your function should return length = 2, with the first two elements of
+# nums being 1 and 2 respectively. It doesn't matter what you leave beyond
+# the new length.
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count =0
+        while count + 1 < len(nums):
+            if nums[count] < nums[count+1]:
+                count = count + 1
+            else:
+                nums.remove(nums[count+1])
+        return len(nums)
