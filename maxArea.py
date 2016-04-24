3e500dec084dc45b1baf1f6f2b3930e5c0229792
Karthik <karthik.dot@gmail.com>
2016-04-23 18:47:24 -0700

    max area using a list of integers and indices - leetcode problem. Solution is brute force, very slow. Optimized solution pending

---
+++ maxArea.py
@@ -0,0 +1,28 @@
+# # Given n non-negative integers a1, a2, ..., an, where each represents a point
+#  at coordinate (i, ai). n vertical lines are drawn such that the two endpoints
+#   of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis
+#    forms a container, such that the container contains the most water.
+# #
+# # Note: You may not slant the container.
+
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        maxAR = 0
+        count = 0
+        while count < len(height):
+            for i in range(len(height)-1, count, -1):
+                if height[i] > height[count]:
+                    minH = height[count]
+                else:
+                    minH = height[i]
+                area = ((i - count) * minH)
+                if area < 0:
+                    area = area * -1
+                if  area > maxAR:
+                    maxAR = area
+            count = count + 1
+        return maxAR
