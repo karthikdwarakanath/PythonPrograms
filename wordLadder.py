32f910a7fadbce27df57bb2810a5a2598cc71d74
Karthik <karthik.dot@gmail.com>
2016-07-08 12:17:55 -0700

    word ladder

--- wordLadder.py
+++ wordLadder.py
@@ -11,16 +11,18 @@
 
 from collections import deque
 class WordNode(object):
-    def __init__(self, word, numSteps):
+    def __init__(self, word, numSteps, pre):
         self.word = word
         self.numSteps = numSteps
+        self.pre = pre
 
 def findLengthOfTransformation(start, end, wordDict):
     bfsQ = deque()
     alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',\
     'q','r','s','t','u','v','w','x','y','z']
-    startWord = WordNode(start, 0)
+    startWord = WordNode(start, 0, None)
     bfsQ.append(startWord)
+    # reached = 100
     while len(bfsQ) > 0:
         top = bfsQ.popleft()
         if top.word == end:
@@ -31,9 +33,10 @@ def findLengthOfTransformation(start, end, wordDict):
                 if c != top.word[i]:
                     str1 = top.word[:i] + c + top.word[i+1:]
                 if str1 in wordDict:
-                    newWord = WordNode(str1, top.numSteps+1)
+                    newWord = WordNode(str1, top.numSteps+1, tmp)
                     bfsQ.append(newWord)
                     del wordDict[str1]
+                    print str1, newWord.numSteps, newWord.pre
                 top.word = tmp
 
-print findLengthOfTransformation('hit', 'cog', {'hot':1, 'dot':1, 'dog':1, 'cog':1})
+print findLengthOfTransformation('hit', 'cog', {'hot':1, 'dot':1, 'dog':1, 'cog':1, 'lot':1, 'log':1, 'bob':1, 'rot':1})
